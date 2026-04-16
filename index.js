4c80d82290ce747bfb802cd227727e96efaf90ef
zyppayx-lab <zyppayx@gmail.com>
2026-04-16 22:11:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const db = require("./db");
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is working");
+  res.send("Server + DB connected");
+});
+
+// TEST DATABASE CONNECTION
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await db.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(3000, () => {
-  console.log("Server started");
+  console.log("Server running");
 });
